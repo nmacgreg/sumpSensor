267c293ccf8b2ed201f98a4cbb33d71e1c645ae1
Neil MacGregor <neilmacgregor7@gmail.com>
2018-11-23 19:17:46 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+I've added 2 scripts that mostly work.  I'm still prototyping. 
+
+distance.py works the best.
+
+The goal will be to get a measurement off the distance sensor.
+# sumpSensor
---
+++ distance.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+try:
+      #GPIO.setmode(GPIO.BOARD)
+      GPIO.setmode(GPIO.BCM)
+
+      #PIN_TRIGGER = 7
+      #PIN_ECHO = 11
+      PIN_TRIGGER = 23
+      PIN_ECHO = 24
+
+      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+      GPIO.setup(PIN_ECHO, GPIO.IN)
+
+      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+      print "Waiting for sensor to settle"
+
+      time.sleep(2)
+
+      print "Strobe the trigger"
+      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+      time.sleep(0.00001)
+      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+      # measuring the time until a response...
+      while GPIO.input(PIN_ECHO)==0:
+                      pulse_start_time = time.time()
+      while GPIO.input(PIN_ECHO)==1:
+                      pulse_end_time = time.time()
+
+      pulse_duration = pulse_end_time - pulse_start_time
+      distance = round(pulse_duration * 17150, 2)
+      print "Distance:",distance,"cm"
+
+finally:
+      GPIO.cleanup()
