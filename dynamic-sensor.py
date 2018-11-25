0543958b8acc2f7c5f79ad20e1ee4ff90b9d3c6e
Neil MacGregor <neilmacgregor7@gmail.com>
2018-11-24 22:53:06 -0700

    Removed the light, but ya, it uses 100% of a CPU, for no good reason

--- dynamic-sensor.py
+++ dynamic-sensor.py
@@ -8,96 +8,6 @@ import random
 import time
 import uuid
 
-
-class OverheatedEvent(Event):
-
-    def __init__(self, thing, data):
-        Event.__init__(self, thing, 'overheated', data=data)
-
-
-class FadeAction(Action):
-
-    def __init__(self, thing, input_):
-        Action.__init__(self, uuid.uuid4().hex, thing, 'fade', input_=input_)
-
-    def perform_action(self):
-        time.sleep(self.input['duration'] / 1000)
-        self.thing.set_property('brightness', self.input['brightness'])
-        self.thing.add_event(OverheatedEvent(self.thing, 102))
-
-
-class ExampleDimmableLight(Thing):
-    """A dimmable light that logs received commands to stdout."""
-
-    def __init__(self):
-        Thing.__init__(self,
-                       'My Lamp',
-                       ['OnOffSwitch', 'Light'],
-                       'A web connected lamp')
-
-        self.add_property(
-            Property(self,
-                     'on',
-                     Value(True, lambda v: print('On-State is now', v)),
-                     metadata={
-                         '@type': 'OnOffProperty',
-                         'label': 'On/Off',
-                         'type': 'boolean',
-                         'description': 'Whether the lamp is turned on',
-                     }))
-
-        self.add_property(
-            Property(self,
-                     'brightness',
-                     Value(50, lambda v: print('Brightness is now', v)),
-                     metadata={
-                         '@type': 'BrightnessProperty',
-                         'label': 'Brightness',
-                         'type': 'integer',
-                         'description': 'The level of light from 0-100',
-                         'minimum': 0,
-                         'maximum': 100,
-                         'unit': 'percent',
-                     }))
-
-        self.add_available_action(
-            'fade',
-            {
-                'label': 'Fade',
-                'description': 'Fade the lamp to a given level',
-                'input': {
-                    'type': 'object',
-                    'required': [
-                        'brightness',
-                        'duration',
-                    ],
-                    'properties': {
-                        'brightness': {
-                            'type': 'integer',
-                            'minimum': 0,
-                            'maximum': 100,
-                            'unit': 'percent',
-                        },
-                        'duration': {
-                            'type': 'integer',
-                            'minimum': 1,
-                            'unit': 'milliseconds',
-                        },
-                    },
-                },
-            },
-            FadeAction)
-
-        self.add_available_event(
-            'overheated',
-            {
-                'description':
-                'The lamp has exceeded its safe operating temperature',
-                'type': 'number',
-                'unit': 'degree celsius',
-            })
-
-
 class GPIODepthSensor(Thing):
     """A humidity sensor which updates its measurement every few seconds."""
 
@@ -158,17 +68,12 @@ class GPIODepthSensor(Thing):
 
 
 def run_server():
-    # Create a thing that represents a dimmable light
-    light = ExampleDimmableLight()
-
     # Create a thing that represents a humidity sensor
     sensor = GPIODepthSensor()
 
     # If adding more than one thing, use MultipleThings() with a name.
     # In the single thing case, the thing's name will be broadcast.
-    server = WebThingServer(MultipleThings([light, sensor],
-                                           'LightAndTempDevice'),
-                            port=8888)
+    server = WebThingServer(MultipleThings([sensor], 'DepthDevice'), port=8888)
     try:
         logging.info('starting the server')
         server.start()
